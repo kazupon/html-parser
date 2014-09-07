d759645393c996ed3c1af9997d4841bdda7efb6e
kazuya kawaguchi <kawakazu80@gmail.com>
2014-09-08 02:18:02 +0900

    Setup basic project files

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * import(s)
+ */
+
+var debug = require('debug')('html-parser')
+
+
+/**
+ * export(s)
+ */
+
+module.exports = parser
+
+function parser () {
+}
---
+++ test/index.js
@@ -0,0 +1,13 @@
+/**
+ * import(s)
+ */
+
+var expect = require('expect.js')
+
+
+/**
+ * test(s)
+ */
+
+describe('html-parser', function () {
+})
